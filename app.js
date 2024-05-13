42e87611bd667fe71d5d1427f08ad94f72c21c86
Allen <lampareroallen2@gmail.com>
2024-05-14 01:34:15 +0800

    added secure config

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const sessionConfig = {
     cookie:{
         domain:'.onrender.com',
         httpOnly: true,
-        // secure:true,
+        secure:true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: 'none'
