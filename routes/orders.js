e6ec6879f2245f0989061058de3a79df9ad0c775
Allen <lampareroallen2@gmail.com>
2024-05-13 23:56:18 +0800

    removed the isAuthenticated method of passport

--- routes/orders.js
+++ routes/orders.js
@@ -4,10 +4,10 @@ const {order} = require('../controller/order')
 const catchAsync = require('../utils/catchAsync')
 const {isLoggedIn} = require('../middleware')
 
-router.post('/',isLoggedIn,catchAsync(order.userOrder))//post an order based on the userId
-router.get('/:userId',isLoggedIn,catchAsync(order.getOrders))
+router.post('/',catchAsync(order.userOrder))//post an order based on the userId
+router.get('/:userId',catchAsync(order.getOrders))
 router.route('/:orderId')
-    .delete(isLoggedIn,catchAsync(order.deleteOrder))
-    .patch(isLoggedIn,catchAsync(order.recieveOrder))
+    .delete(catchAsync(order.deleteOrder))
+    .patch(catchAsync(order.recieveOrder))
 
 module.exports = router
