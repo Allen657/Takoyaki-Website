6d45e6beb6e62c45353f655a2fb123defcad117b
Allen <lampareroallen2@gmail.com>
2024-05-14 12:15:50 +0800

    s

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(cors({
     origin:'https://takoyaki-1.onrender.com',
     credentials:true
 }))
-
+app.set('trust proxy',1)
 app.use(session(sessionConfig))
 app.use(passport.session());
 passport.use(new passportLocal(User.authenticate()));
