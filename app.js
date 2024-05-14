5ea5c22a17bf97db66d1ed0c2e665289d81e934c
Allen <lampareroallen2@gmail.com>
2024-05-14 11:30:21 +0800

    a

--- app.js
+++ app.js
@@ -36,8 +36,10 @@ const sessionConfig = {
     resave:false,
     saveUninitialized:true,
     cookie:{
+        httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
+        secure:true,
         sameSite:'none'
     }
 }
